928a51509b758435518d106321e4251e69e35a66
Ganesh-P03 <84033688+Ganesh-P03@users.noreply.github.com>
2024-03-29 15:13:53 +0530

    added rpc

--- smartcontract/hardhat.config.js
+++ smartcontract/hardhat.config.js
@@ -7,11 +7,8 @@ module.exports = {
   solidity: { version: "0.8.20" },
   networks: {
     testnet: {
-      url: `https://testnet-rpc.Botanix.network`,
+      url: `https://node.botanixlabs.dev`,
       accounts: [`0x${privateKey}`],
     },
-    ganache: {
-      url: `http://127.0.0.1:7545`,
-    },
-  }
+  },
 };
